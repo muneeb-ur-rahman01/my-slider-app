f06bbaa4aed19249024a4dacc36a50a1bd685547
warishak56 <warishaaamir57@gmail.com>
2025-04-05 12:23:53 +0500

    initial-commit

---
+++ src/Slider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Slider = () => {
+    return (
+        <>
+            <div className="slider">
+                {/* Your animation/image logic here */}
+                <h2>Cool Slider</h2>
+            </div>
+        </>
+    )
+}
+
+export default Slider
